c72c0e8068984a639b7da1eb6fd7ef9605ca6e11
rona <ssauapw@qq.com>
2016-02-14 11:31:34 +0800

    [WIP]add the module sum_of_qty_and_final_qty.

---
+++ sum_of_qty_and_final_qty/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# © 2016 Elico Corp (www.elico-corp.com).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sum Of Qty And Final Qty",
+    "summary": "Module summary",
+    "version": "7.0.1.0.1",
+    "category": "Stock",
+    "website": "www.elico-corp.com",
+    "author": "Elico corp",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "sale",
+        "stock",
+        "purchase",
+    ],
+    "data": [
+        'views/stock_view.xml',
+        'views/purchase_view.xml',
+        'views/sale_view.xml',
+    ]
+
+}
