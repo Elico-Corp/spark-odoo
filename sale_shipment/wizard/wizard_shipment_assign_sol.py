0a40cc0df6f3d458437e0513a702e25f2b611e74
victor <victor.martin@elico-corp.com>
2016-04-18 15:31:33 +0800

    [FIX] : sale_shipment: Fix or reply all GitHib comments, fix wizard_shipment_assign_sol.py

--- sale_shipment/wizard/wizard_shipment_assign_sol.py
+++ sale_shipment/wizard/wizard_shipment_assign_sol.py
@@ -82,17 +82,17 @@ class WizardShipmentAssignSOL(orm.TransientModel):
         res['domain'] = {'sol_ids': domain}
         return res
 
-    def _check_product_ids(self, wizard):
+    def _is_product_ids_unique(self, wizard):
         """
         Check the product ids.
         Products in SOL assigned to shipment, should be different.
         """
-        p_list = [sol.product_id.id for sol in wizard.sol_ids]
-        if len(p_list) > len(Counter(p_list).values()):
+        if len(
+                [sol.product_id.id for sol in wizard.sol_ids]) > len(
+                    Counter([sol.product_id.id for sol in wizard.sol_ids])):
             raise orm.except_orm(
                 _('Error!'),
-                _('Must have different products per sale order line'))
-        p_list.append(sol.product_id.id)
+                _('You cannot have the same product twice'))
         return True
 
     def shipment_assign_sol(self, cr, uid, ids, context=None):
@@ -104,7 +104,7 @@ class WizardShipmentAssignSOL(orm.TransientModel):
         active_id = context.get('active_id', False)
         if not active_id:
             return False
-        if self._check_product_ids(wizard):
+        if self._is_product_ids_unique(wizard):
             for sol in wizard.sol_ids:
                 # to be compatible with inter company API
                 if sol.order_id:
