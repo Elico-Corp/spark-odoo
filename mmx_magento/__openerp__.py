9fbd423d4753dcc0a1e8706f805efce4b95c6c50
rona <ssauapw@qq.com>
2016-04-21 11:00:27 +0800

    [IMP]make connector manager inherit from connector user.

--- mmx_magento/__openerp__.py
+++ mmx_magento/__openerp__.py
@@ -22,7 +22,7 @@
 
 {
     'name': 'MMX Magento',
-    'version': '7.0.1.0.1',
+    'version': '7.0.1.0.2',
     'category': 'Connector',
     'depends': ['account',
                 'magentoerpconnect',
