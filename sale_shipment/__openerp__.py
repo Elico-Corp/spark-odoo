0f68669a937771942a8810d8d4900da2ebf8021e
rona <ssauapw@qq.com>
2016-05-10 17:08:31 +0800

    [FIX]fix the conflict.

--- sale_shipment/__openerp__.py
+++ sale_shipment/__openerp__.py
@@ -24,7 +24,7 @@
 
 {
     'name': 'Sale Shipment',
-    'version': '7.0.1.1.8',
+    'version': '7.0.1.2.0',
     'author': 'Elico Corp',
     'website': 'http://www.elico-corp.com',
     'summary': '',
