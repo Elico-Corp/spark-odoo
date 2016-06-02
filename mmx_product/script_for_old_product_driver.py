28a5bf0b08d36961858a11392178e5b68b7dbff5
rona <ssauapw@qq.com>
2016-06-02 16:12:57 +0800

    [IMP]improve the script.

--- mmx_product/script_for_old_product_driver.py
+++ mmx_product/script_for_old_product_driver.py
@@ -1,10 +1,35 @@
+'''
+Basic script for all the odoo scripts with the following options:
+
+-s (--server): the odoo server to connect.
+-d: database
+-u: user
+-p: password
+'''
+import optparse
 import erppeek
 
+parser = optparse.OptionParser()
+parser.add_option(
+    '-s', '--server', default=None)
+parser.add_option('-d', '--database', help='database')
+parser.add_option('-u', '--user', default=None, help='username')
+parser.add_option(
+    '-p', '--password', default=None, help='password, or it will be \
+    requested on login')
 
-SERVER = 'https://trunk.sparkmodel.com'
-DADABASE = 'stable_mmx'
-USER = 'admin'
-PASSWORD = 'MMX3licoC0rp'
+(args, domain) = parser.parse_args()
+
+
+SERVER = args.server
+DADABASE = args.database
+USER = args.user
+PASSWORD = args.password
+
+print SERVER
+print DADABASE
+print USER
+print PASSWORD
 
 client = erppeek.Client(SERVER, DADABASE, USER, PASSWORD)
 dd = client.model('product.driver').search([])
